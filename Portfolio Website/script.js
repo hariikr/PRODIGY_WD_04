e27a366fe410bb6ab7913121ad824c1ae2594b2b
Harikrishnan <90984013+hariikr@users.noreply.github.com>
2024-06-23 17:16:13 +0530

    Add files via upload

---
+++ Portfolio Website/script.js
@@ -0,0 +1,12 @@
+   var typed = new Typed(".auto-input", {
+    strings: [" , I am"],
+    typeSpeed:105,
+    showCursor:false,
+    })
+    var typed = new Typed(".auto-input2", {
+    strings: ["^150","Harikrishnan"],
+    typeSpeed:145,
+    showCursor:false,
+    })
+
+   
